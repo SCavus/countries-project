e32cbb9c6a5ec9facc9eb81b894b11025e4f9951
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-05-09 14:33:14 +0100

    update

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function setup() {
         .then((data) =>{
             console.log(data)
             displayCountries(data)
-        })        
+        })
+   // searchByRegion()
+   let regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania']     
+   let filter = document.querySelector('#filter-region')
+   regions.map = ((region) => {
+       let option = `<option class="option">${region}</option>`
+       filter.innerHTML += option
+   })
 }
 
 function displayCountries(countryData) {
@@ -21,28 +28,43 @@ function displayCountries(countryData) {
         <p class="info">Capital: ${element.capital}</p>`
         mainPage.appendChild(country)
     })
-} 
+}
+function displaySelectedCountry(countryData) {
+    const display = document.querySelector('#display')
+    display.innerHTML = ""
+    country.className = "single-country"
+    country.innerHTML = `
+    <div><button>Back</button></div>
+    <div>
+        <img class="flag-single" src=${element.flag}>
+        <div>
+            <div>
+                <h3 class="country-name">${element.name}</h3>
+                <p class="info-single">Native Name: ${element.population}</p>
+                <p class="info-single">Population: ${element.population}</p>
+                <p class="info-single">Region: ${element.region}</p>
+                <p class="info-single">Sub Region: ${element.region}</p>
+                <p class="info-single">Capital: ${element.capital}</p>
+                <p class="info-single">Top Level Domain: ${element.region}</p>
+                <p class="info-single">Currencies: ${element.region}</p>
+                <p class="info-single">Languages: ${element.region}</p>
+            </div>
+            <div>
 
-function displayPage(list) {
-    const container = document.getElementById("container");
-    container.innerHTML = "";
-    list.forEach(element => {
-      const episode = document.createElement("div");
-      episode.innerHTML = `
-      <h3 class='title'>${element.name} - S${element.season < 10 ? 0 : ""} ${
-        element.season
-      } E${element.number < 10 ? 0 : ""} ${element.number}</h3>
-      <img class='image' src=${element.image.medium} />
-      <p class='summary'>${element.summary}</p>
-      `;
-      container.appendChild(episode);
-    });
+            </div>
+        </div>
+    </div>`
+        mainPage.appendChild(country)
+    
+} 
+function searchByRegion() {
+    let regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania']     
+    let filter = document.querySelector('#filter-region')
+    regions.forEach = ((region) => {
+        let option = `<option>${region}</option>`
+        filter.innerHTML += option
+    })
 }
 
-//   const rootElem = document.getElementById("root");
-//   rootElem.innerHTML = `Search ${episodeList.length} episodes
-                    
-//                         <select id="episode-list"> </select>
-//                         <input type="search" id="site-search" placeholder="Search item">`; //Create search field
 
 window.onload = setup
