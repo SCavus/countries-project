2f9eea4a08645a8bbc02a42fdc26a3a0813fc70c
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-05-09 02:14:18 +0100

    update

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ function displayCountries(countryData) {
     mainPage.innerHTML = ""
     countryData.forEach(element => {
         const country = document.createElement("div")
+        country.className = "country"
         country.innerHTML = `
         <img class="flag" src=${element.flag}>
         <h3 class="country-name">${element.name}</h3>
-        <p class="info">${element.population}</p>
-        <p class="info">${element.region}</p>
-        <p class="info">${element.capital}</p>`
+        <p class="info">Population: ${element.population}</p>
+        <p class="info">Region: ${element.region}</p>
+        <p class="info">Capital: ${element.capital}</p>`
         mainPage.appendChild(country)
     })
 } 
