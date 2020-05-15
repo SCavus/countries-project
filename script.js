4a71a873ea6261c6e8abcda6b674ae07549d6304
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-05-15 23:56:56 +0100

    update-borders

--- script.js
+++ script.js
@@ -53,71 +53,46 @@ function displayCountries(countryData) {
 
 function displayDetail(countryId) {
   document.querySelector("#display").className += "hide";
-  let selectedCountry = allCountries.filter(
+  let selectedCountry = allCountries.find(
     (element) => countryId === element.alpha3Code
   );
+  let currency = selectedCountry.currencies.map((el) => (el = el.name));
+  let language = selectedCountry.languages.map((el) => (el = el.name));
+  let borderCountries = selectedCountry.borders;
+
   const countryDetailPage = document.querySelector("#country-details-page");
   countryDetailPage.innerHTML = `
         <div><button>Back</button></div>
         <div class="single-country">
-          <img class="flag-single" src=${selectedCountry[0].flag}>
+          <img class="flag-single" src=${selectedCountry.flag}>
           <div class="info-container">
-            <h3 class="country-name">${selectedCountry[0].name}</h3>
+            <h3 class="country-name">${selectedCountry.name}</h3>
             <div class="infoblock-single">
-              <p class="info-single">Native Name: ${selectedCountry[0].nativeName}</p>
-              <p class="info-single">Population: ${selectedCountry[0].population}</p>
-              <p class="info-single">Region: ${selectedCountry[0].region}</p>
-              <p class="info-single">Sub Region: ${selectedCountry[0].subregion}</p>
-              <p class="info-single">Capital: ${selectedCountry[0].capital}</p>
-              <p class="info-single">Top Level Domain: ${selectedCountry[0].topLevelDomain}</p>
-              <p class="info-single">Currencies: ${selectedCountry[0].currencies}</p>
-              <p class="info-single">Languages: ${selectedCountry[0].languages}</p>
+              <p class="info-single">Native Name: ${selectedCountry.nativeName}</p>
+              <p class="info-single">Population: ${selectedCountry.population}</p>
+              <p class="info-single">Region: ${selectedCountry.region}</p>
+              <p class="info-single">Sub Region: ${selectedCountry.subregion}</p>
+              <p class="info-single">Capital: ${selectedCountry.capital}</p>
+              <p class="info-single">Top Level Domain: ${selectedCountry.topLevelDomain}</p>
+              <p class="info-single">Currencies: ${currency}</p>
+              <p class="info-single">Languages: ${language}</p>
             </div>
-            <div>
+            <div id="borders">
                 <h4>Border Countries</h4>
             </div>
           </div>
         </div>`;
-  let button = document.querySelector('button')
-  button.addEventListener('click', ()=> {
-    countryDetailPage.className+= "hide"
-    document.querySelector("#display").className += "show";
+  let border = document.querySelector("#borders");
+  borderCountries.map((el) => {
+    border.innerHTML += `<button>${el}</button>`;
+  });
 
-  })
+  let button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    countryDetailPage.className += "hide";
+    document.querySelector("#display").className += "show";
+  });
 }
-// function displayDetail() {
-//   mainPage.innerHTML = ""
-//   console.log(countryId);
-//   let selectedCountry = allCountries.filter((element) => {
-//     if (countryId === element.name) {
-//       return element
-//     }})
-//     console.log(selectedCountry)
-//     const display = document.querySelector("#display");
-//     country.className = "single-country";
-//     country.innerHTML = `
-//         <div><button>Back</button></div>
-//         <div>
-//             <img class="flag-single" src=${selectedCountry[0].flag}>
-//             <div>
-//                 <div>
-//                     <h3 class="country-name">${selectedCountry[0].name}</h3>
-//                     <p class="info-single">Native Name: ${selectedCountry[0].nativeName}</p>
-//                     <p class="info-single">Population: ${selectedCountry[0].population}</p>
-//                     <p class="info-single">Region: ${selectedCountry[0].region}</p>
-//                     <p class="info-single">Sub Region: ${selectedCountry[0].subregion}</p>
-//                     <p class="info-single">Capital: ${selectedCountry[0].capital}</p>
-//                     <p class="info-single">Top Level Domain: ${selectedCountry[0].topLevelDomain}</p>
-//                     <p class="info-single">Currencies: ${selectedCountry[0].currencies}</p>
-//                     <p class="info-single">Languages: ${selectedCountry[0].languages}</p>
-//                 </div>
-//                 <div>
-//                     <h4>Border Countries</h4>
-//                 </div>
-//             </div>
-//         </div>`;
-//     display.appendChild(country);
-//   }
 
 function searchTerm(countryList) {
   const search = document.querySelector("#search");
